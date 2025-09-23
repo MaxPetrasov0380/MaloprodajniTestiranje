915a5e0f1c79917671cdea4453f9c1c358fd916b
MaxPetrasov0380 <zwackysa2@gmail.com>
2025-09-23 16:39:03 +0200

    minimalne promene

---
+++ MaloprodajniObjekatCs-main/InventarIntegrationTesting/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace InventarIntegrationTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ MaloprodajniObjekatCs-main/KasaE2E/UnitTest1.cs
@@ -0,0 +1,20 @@
+using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Definitions;
+using FlaUI.UIA3;
+using System;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace KasaE2E
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
